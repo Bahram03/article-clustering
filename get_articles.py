19e5a576a8b12557d06c58c1f8845ca083ebb5ef
Mammad-Smoke <mohammadbahrami82@outlook.com>
2024-06-25 15:22:12 +0330

    refactor: get and save the files into assets

--- get_articles.py
+++ get_articles.py
@@ -1,16 +1,12 @@
 import csv
-import time
-
 import requests
 from bs4 import BeautifulSoup
-
 from find_articles_links import read_links_from_file
-
 
 def get_fields(links, base: str, writer):
     for i, link in enumerate(links):
         soup = BeautifulSoup(requests.get(link).content, 'html.parser')
-        print(i)
+        print(f"Processing article {i+1}/{len(links)}")
         # Extracting title
         try:
             title = soup.find('span', class_='article_title bold').text.strip()
@@ -18,9 +14,7 @@ def get_fields(links, base: str, writer):
             # Extracting authors
             list_a = soup.find('ul', class_='list-inline list-inline-seprator margin-bottom-6 rtl') \
                 .find_all('li', class_='padding-3')
-            authors = list()
-            for a in list_a:
-                authors.append(a.find('a').text)
+            authors = [a.find('a').text for a in list_a]
 
             # Extract abstract
             abstract = soup.find('div', class_='padding_abstract justify rtl').text.strip()
@@ -33,8 +27,10 @@ def get_fields(links, base: str, writer):
             link = soup.find('ul', class_='list-group list-group-bordered list-group-noicon nomargin').find_all('a')[1].get(
                 'href')
             link = f'{base}{link[1:]}'
-        except:
+        except AttributeError:
+            print(f"Skipping article {i+1} due to missing information")
             continue
+        
         article_crawled = {
             'Title': title,
             'Authors': authors,
@@ -44,16 +40,15 @@ def get_fields(links, base: str, writer):
         }
         writer.writerow(article_crawled)
 
-
 if __name__ == '__main__':
-    csv_filename = 'crawled_articles.csv'
-    csvfile = open(csv_filename, 'a', newline='', encoding='utf-8')
-    fieldnames = ['Title', 'Authors', 'Link', 'Abstract', 'Keywords']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    base_url = "https://jte.ut.ac.ir"
-    article_links_filename = 'article_links.txt'
-    articles_links = read_links_from_file(article_links_filename)
-    get_fields(articles_links, base_url, writer)
-
-
-
+    csv_filename = 'assets/crawled_articles.csv'
+    article_links_filename = 'assets/article_links.txt'
+    
+    with open(csv_filename, 'a', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Title', 'Authors', 'Link', 'Abstract', 'Keywords']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        
+        base_url = "https://jte.ut.ac.ir"
+        articles_links = read_links_from_file(article_links_filename)
+        get_fields(articles_links, base_url, writer)
